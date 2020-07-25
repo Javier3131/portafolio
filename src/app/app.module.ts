57ea59d855d052a63b786e95abf5768956c8a70f
Javier Lopez <javierl.1994@gmail.com>
2020-07-25 02:39:05 -0600

    Init repository.

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      SideBarComponent,
+      PortfolioComponent,
+      AboutMeComponent,
+      ContactComponent,
+      HeaderComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
