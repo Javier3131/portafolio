d058a23db1be0854f3102a6b20b6d30fc415664e
Javier Lopez <javierl.1994@gmail.com>
2020-07-30 00:24:30 -0600

    * Add asp mobile card. * Add footer. * Start developing contact form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
    declarations: [
@@ -18,7 +19,8 @@ import { HeaderComponent } from './header/header.component';
       PortfolioComponent,
       AboutMeComponent,
       ContactComponent,
-      HeaderComponent
+      HeaderComponent,
+      FooterComponent
    ],
    imports: [
       BrowserModule,
